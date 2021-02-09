9ad9555031d0a39add572a0b7182147f0104ccb3
Egor Lipskiy <elipskiy@yandex-team.ru>
2021-02-09 15:41:49 +0300

    Implement search api

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -1,5 +1,29 @@
+import Fuse from 'fuse.js';
+import streets from '../db/streets';
+
 export async function getTestData(req, res) {
   res.status(200).json({
     success: true,
-  })
+  });
+}
+
+export async function search(req, res) {
+  console.log(req.query.search);
+  const {
+    limit = 1,
+    query: search,
+  } = req.query;
+
+  const fuse = new Fuse(streets, {
+    includeScore: true,
+    threshold: 0.3,
+    keys: ['type', 'name'],
+  });
+
+  const result = fuse.search(req.query.search);
+  console.log(result);
+
+  res
+    .status(200)
+    .json(result.splice(0, Math.max(1, limit)));
 }
--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getTestData } from '../controllers/api';
+import { getTestData, search } from '../controllers/api';
 
 const router = Router();
 router
-  .get(`/test`, getTestData);
+  .get(`/test`, getTestData)
+  .get('/search', search);
 
 export default {
   use: '/api/v1',
